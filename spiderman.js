6ab1bdb3ecd21a65e21df10a5c2d0e9f749b264a
Rissa <71231854+mgimbel12@users.noreply.github.com>
2025-08-22 09:39:47 -0700

    Update spiderman.js

--- spiderman.js
+++ spiderman.js
@@ -2,7 +2,7 @@ function checkPassword() {
   const input = document.getElementById("vault-pass").value;
   const message = document.getElementById("vault-msg");
   if (input === "SpideyChristmas") {
-    message.innerHTML = `
+    message.innerHTML= `
       <p>✅ Welcome to the Spider-Verse Vault!</p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/lBdbttgfmRw?si=Gg4UmNkWoEc9zz9g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/zAd1QOKO0sA?si=WXEvyt_nsEC1aDw5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -10,7 +10,7 @@ function checkPassword() {
 <iframe width="560" height="315" src="https://www.youtube.com/embed/JQ93n1HQIIk?si=ltP3jnkmWKDj_eGl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  
  <iframe width="560" height="315" src="https://www.youtube.com/embed/mzWH-M_S7ow?si=fjNoLU8mbvclOKo3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   <iframe width="560" height="315" src="https://www.youtube.com/embed/I_s4lX_ECLM?si=aZFCHHonXzuvBx-Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  
-    ';
+`;
   } else {
     message.innerHTML = "<p>❌ Incorrect password. Try again!</p>";
   }
