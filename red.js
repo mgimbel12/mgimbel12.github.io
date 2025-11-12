6cc132433668fbf56ba5f857ac1c97111d16f98e
Rissa <71231854+mgimbel12@users.noreply.github.com>
2025-11-11 17:47:14 -0800

    Update red.js

--- red.js
+++ red.js
@@ -1,4 +1,7 @@
 document.getElementById('tributeBtn').addEventListener('click', () => {
   alert('Thank you to all who serve. 🇺🇸');
 });
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Honoring our heroes through tribute and remembrance 🇺🇸');
+});
 
